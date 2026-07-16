36a3549c3a95101bae4eadb7c13116e882ee6e53
bhavyabajpai02 <bhavyabajpai79@gmail.com>
2026-07-16 23:16:23 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -9,26 +9,46 @@
  * };
  */
 class Solution {
-public:
+public: 
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode*ptr1 = list1,*ptr2 = list2, *next1 = list1->next, *next2 = list2->next;
+        while(next1 and ptr2){
+            next2 = ptr2->next;
+            if(ptr1->val <= ptr2->val and ptr2->val < next1->val){
+                ptr2->next = ptr1->next;
+                ptr1->next = ptr2;
+                ptr1 = ptr2; 
+                ptr2 = next2;
+            }
+            else{
+                ptr1 = next1;
+                next1= next1->next;
+                if(!next1){
+                    ptr1->next = ptr2;
+                    return list1;
+                }
+            }
+        }
+        
+        
+        return list1;
+
+    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        ListNode* ptr1 = list1;
-        ListNode* ptr2 = list2;
-        while(ptr1 != nullptr){
-            pq.push(ptr1->val);
-            ptr1 = ptr1->next;
+        if(!list1) return list2;
+        if(!list2) return list1;
+        if(list1->val<= list2->val){
+            if(list1->next == nullptr) {
+                list1->next = list2;
+                return list1;
+            }
+           return  merge(list1,list2);
+        } 
+        if(list2->next == nullptr){
+            list2->next = list1;
+            return list2;
         }
-        while(ptr2 != nullptr){
-            pq.push(ptr2->val);
-            ptr2 = ptr2->next;
-        }
-        ListNode* dummy = new ListNode();
-        ListNode* ptr = dummy;
-        while(!pq.empty()){
-            ptr->next = new ListNode(pq.top());
-            pq.pop();
-            ptr = ptr->next;
-        }
-        return dummy->next;
+            return merge(list2,list1);
+        
     }
 };
