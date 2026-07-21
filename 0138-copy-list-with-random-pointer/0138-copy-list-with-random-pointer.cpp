eb2d6caf6360a106ea04041e2d2c3bb3f8aa3b64
bhavyabajpai02 <bhavyabajpai79@gmail.com>
2026-07-21 20:56:23 +0530

    Time: 5 ms (64.54%), Space: 14.9 MB (86.25%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,33 +17,81 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* ptr = head;
-        // Inserting new Nodes
+        // Node* ptr = head;
+        // // Inserting new Nodes
+        // while(ptr){
+        //     Node* node = new Node(ptr->val);
+        //     node->next = ptr->next;
+        //     ptr->next = node;
+        //     ptr = node->next;
+        // }
+        // // Updating random pointers
+        // ptr = head;
+        // while(ptr){
+        //     if(ptr->random)
+        //     ptr->next->random = ptr->random->next;
+        //     ptr = ptr->next->next;
+        // }
+
+        // // Seperating nodes
+        // Node* newHead = head?head->next:nullptr;
+        // ptr = head;
+        // while(ptr){
+        //     Node* copy = ptr->next;
+        //     ptr->next = copy->next;
+        //     if(copy->next)
+        //     copy->next = copy->next->next;
+        //     ptr = ptr->next;
+        // }
+        // return newHead;
+
+        // new List
+        Node* newHead = nullptr,*curr = nullptr,*ptr = head;
         while(ptr){
-            Node* node = new Node(ptr->val);
-            node->next = ptr->next;
-            ptr->next = node;
-            ptr = node->next;
+            if(!newHead){
+                newHead = new Node(ptr->val);
+                curr = newHead;
+            }
+            else{
+                curr->next = new Node(ptr->val);
+                curr = curr->next;
+            }
+            ptr = ptr->next;
         }
-        // Updating random pointers
+
+        // combining new + orginal list 
+        curr = newHead;
+        ptr = head;
+        Node* nextcopy = nullptr,*nextorg = nullptr;
+        while(ptr){
+            nextorg = ptr->next;
+            ptr->next = curr;
+            ptr = nextorg;
+            nextcopy = curr->next;
+            curr->next = ptr;
+            curr = nextcopy;
+        }
+
+        // setting random pointers
         ptr = head;
         while(ptr){
-            if(ptr->random)
-            ptr->next->random = ptr->random->next;
+            if(ptr->random){
+                ptr->next->random = ptr->random->next;
+            }
             ptr = ptr->next->next;
         }
 
-        // Seperating nodes
-        Node* newHead = head?head->next:nullptr;
+        // splitting new and org
         ptr = head;
+        curr = newHead;
         while(ptr){
-            Node* copy = ptr->next;
-            ptr->next = copy->next;
-            if(copy->next)
-            copy->next = copy->next->next;
+            ptr->next = curr->next;
             ptr = ptr->next;
+            if(ptr){
+                curr->next = ptr->next;
+            }
+                curr = curr->next;
         }
         return newHead;
-
     }
 };
